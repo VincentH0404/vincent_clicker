4ff58c82139288386b585fabaa5a9dae1bc6bf35
VincentH0404 <146112123+VincentH0404@users.noreply.github.com>
2024-10-18 15:36:19 -0400

    Add files via upload

--- vincent_clicker/index.js
+++ vincent_clicker/index.js
@@ -1,10 +1,41 @@
-document.getElementById("count-el").innerText = 5
+document.getElementById("count-el").innerText = 0
 let t = document.createElement("title")
 t.innerHTML = "Clicker"
 document.head.appendChild(t)
 
 let count = 0
+let base_count = 1
+let winning_num = Math.floor(Math.random() * 20 + base_count) 
 function increment() {
     count = count + 1
     document.getElementById("count-el").innerText = count
+
+    if (count % 10 == 0) {
+        document.body.setAttribute("style", "filter: drop-shadow(32px 32px 40px blue) invert(205%);", "filter: hue-rotate(90deg);")
+    }
+    else {
+        document.body.setAttribute("style", "filter: drop-shadow(0px 0px 0px white) invert(0%);", "filter: hue-rotate(0deg);")
+    }
+    if (count = winning_num) {
+        document.getElementById("popup").setAttribute("style", "display: contents;")
+    }
+    else {
+        document.getElementById("popup").setAttribute("style", "display: none;")
+    }
+    if (count % 20 == 0) {
+        winning_num = Math.floor(Math.random() * 20 + base_count)
+    }
+    
 }
+
+
+let previous_str = document.getElementById("Previous")
+let previous_count = 0
+function save() {
+    if (count != previous_count) {
+        previous_str.innerHTML += count + " - "
+        previous_count = count
+    }
+
+}
+
