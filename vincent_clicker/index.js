590c9c5377c9afc48e87cca3afed1e402a7ce210
VincentH0404 <146112123+VincentH0404@users.noreply.github.com>
2024-10-24 14:52:19 -0400

    Update index.js

--- vincent_clicker/index.js
+++ vincent_clicker/index.js
@@ -16,13 +16,14 @@ function increment() {
     else {
         document.body.setAttribute("style", "filter: drop-shadow(0px 0px 0px white) invert(0%);", "filter: hue-rotate(0deg);")
     }
-    if (count = winning_num) {
+    if (count == winning_num) {
         document.getElementById("popup").setAttribute("style", "display: contents;")
     }
     else {
         document.getElementById("popup").setAttribute("style", "display: none;")
     }
     if (count % 20 == 0) {
+        base_count += 20
         winning_num = Math.floor(Math.random() * 20 + base_count)
     }
     
@@ -36,6 +37,10 @@ function save() {
         previous_str.innerHTML += count + " - "
         previous_count = count
     }
+
+}
+
+
 
 }
 
